c6e9ff3cf77cf38504a76d3140f0e26308403aca
Smandal-physna <66847200+Smandal-physna@users.noreply.github.com>
2020-07-28 16:03:29 -0400

    first commit

---
+++ image2meshinelearning/Controllers/FileUpload.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace image2meshinelearning.Controllers
+{
+
+    //[Route("api/image2meshinelearning")]
+    //[ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult File(IFormFile file)
+        {
+            //Console.WriteLine
+            IFormFile file2 = null;
+            //var file = Request.Form.Files[0];
+            //await _context.SaveChangesAsync();
+            if (file != null)
+            {
+                //work
+                Console.WriteLine("read file");
+                var tmpPath = ".\\abc.png";
+                using (var fileStream = System.IO.File.Exists(tmpPath) ? System.IO.File.Open(tmpPath, FileMode.Append) : System.IO.File.Open(tmpPath, FileMode.CreateNew))
+                {
+                    file.CopyTo(fileStream);
+                }
+                //run_pyt("..\\..\\abc.png", "");
+                Console.WriteLine("ran");
+            }
+            else
+            {
+                Console.WriteLine("is null");
+            }
+            Console.WriteLine(".");
+            return null;
+        }
+    }
+}
---
+++ image2meshinelearning/wwwroot/js/site.js
@@ -0,0 +1,45 @@
+﻿//const uri = 'api/image2meshinelearning';
+function addItem() {
+    var files = document.getElementById('pic').files;
+    var formData = new FormData();
+    formData.append('file', files[0]);
+    console.log(formData)
+    let chunkPath = '/FileUpload/File';
+    const uri = '/FileUpload/File';
+    fetch(uri, {
+        //url: chunkPath,
+        enctype: "multipart/form-data",
+        type: "POST",
+        data: formData,
+        processData: false,
+        contentType: false
+    });
+    //    .done(function (data) {
+    //    // data is return information
+    //});
+    //fetch(uri, {
+    //    //url: '/Image/File',
+    //    url: '@Url.Action("File")',
+    //    method: 'POST',
+    //    body: formData,
+    //    processData: false,
+    //    contentType: false,
+    //    success: function (result) {
+    //    }
+    //});
+   }
+
+    //fetch(uri, {
+    //    method: 'POST',
+    //    headers: {
+    //        'Accept': 'application/json',
+    //        'Content-Type': 'application/json; charset=utf-8'//'application/json'
+    //    },
+    //    body: item
+    //})
+    //    .then(response => response.json())
+    //    .then(() => {
+    //        getItems();
+    //        addNameTextbox.value = '';
+    //    })
+    //        .catch(error => console.error('Unable to add item.', error));
